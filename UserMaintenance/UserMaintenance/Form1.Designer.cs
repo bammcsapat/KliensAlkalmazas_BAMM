68f8612664b365f6f8a334a290e8350f9c6be46a
Amrich Csongor <csongor.amrich@stud.uni-corvinus.hu>
2024-04-15 11:13:05 +0200

    User Interface kezdeti állapot 1.1

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -29,15 +29,22 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            pictureBox1 = new PictureBox();
             button1 = new Button();
             panel2 = new Panel();
+            textBox1 = new TextBox();
             panel3 = new Panel();
+            dataGridView1 = new DataGridView();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.DeepSkyBlue;
+            panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(button1);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
@@ -45,9 +52,19 @@
             panel1.Size = new Size(250, 461);
             panel1.TabIndex = 0;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.MicrosoftTeams_image__1_;
+            pictureBox1.Location = new Point(64, 31);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(120, 80);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
             // button1
             // 
             button1.BackColor = Color.SteelBlue;
+            button1.FlatAppearance.BorderSize = 0;
             button1.FlatStyle = FlatStyle.Flat;
             button1.ForeColor = SystemColors.ControlText;
             button1.Location = new Point(0, 133);
@@ -60,11 +77,23 @@
             // 
             // panel2
             // 
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(textBox1);
             panel2.Dock = DockStyle.Left;
             panel2.Location = new Point(250, 0);
             panel2.Name = "panel2";
             panel2.Size = new Size(500, 461);
             panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Window;
+            textBox1.ForeColor = SystemColors.ControlDark;
+            textBox1.Location = new Point(6, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(488, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Keresés...";
             // 
             // panel3
             // 
@@ -73,6 +102,15 @@
             panel3.Name = "panel3";
             panel3.Size = new Size(239, 461);
             panel3.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 231);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(488, 227);
+            dataGridView1.TabIndex = 1;
             // 
             // Form1
             // 
@@ -87,6 +125,10 @@
             Name = "Form1";
             Text = "Form1";
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
@@ -96,5 +138,8 @@
         private Panel panel2;
         private Panel panel3;
         private Button button1;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private DataGridView dataGridView1;
     }
 }
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+
 namespace UserMaintenance
 {
     public partial class Form1 : Form
@@ -5,6 +7,50 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+
+            // Handle focus events
+            textBox1.GotFocus += (sender, e) =>
+            {
+                if (textBox1.Text == "Keresés...")
+                {
+                    textBox1.Text = "";
+                    textBox1.ForeColor = System.Drawing.Color.Black; // Set text color to black
+                }
+            };
+
+            textBox1.LostFocus += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    textBox1.Text = "Keresés...";
+                    textBox1.ForeColor = System.Drawing.Color.Gray; // Set text color to gray
+                }
+            };
+
+                //// Create rounded corners for the textbox
+                //textBox1.Padding = new Padding(10, 5, 10, 5); // Add padding to the textbox
+                //GraphicsPath path = new GraphicsPath();
+                //path.AddArc(0, 0, 20, 20, 180, 90);
+                //path.AddArc(textBox1.Width - 20, 0, 20, 20, 270, 90);
+                //path.AddArc(textBox1.Width - 20, textBox1.Height - 20, 20, 20, 0, 90);
+                //path.AddArc(0, textBox1.Height - 20, 20, 20, 90, 90);
+                //path.CloseFigure();
+
+                //textBox1.Region = new Region(path);
+
+                //// Create shadow effect
+                //Panel shadowPanel = new Panel();
+                //shadowPanel.Location = new Point(textBox1.Location.X + 3, textBox1.Location.Y + 3);
+                //shadowPanel.Size = textBox1.Size;
+                //shadowPanel.BackColor = Color.Gray;
+                //shadowPanel.BorderStyle = BorderStyle.None;
+                //shadowPanel.Region = new Region(path);
+
+                //panel2.Controls.Add(shadowPanel); // Add shadow panel behind textbox
         }
+
+        
     }
 }
+
+        
